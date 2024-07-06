4c0b77abb08335ed742fea652a4733d5a0f3f5f3
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-07-06 11:10:00 -0300

    Add default loading composable

---
+++ app/src/main/java/com/stahlt/cycleconfigurator/ui/utils/composable/DefaultLoading.kt
@@ -0,0 +1,47 @@
+package com.stahlt.cycleconfigurator.ui.utils.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.stahlt.cycleconfigurator.ui.theme.CycleConfiguratorTheme
+
+@Composable
+fun DefaultLoading(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = Modifier.size(60.dp)
+        )
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = text,
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
+}
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+fun DefaultLoadingPreview(modifier: Modifier = Modifier) {
+    CycleConfiguratorTheme {
+        DefaultLoading(text = "Loading")
+    }
+}
