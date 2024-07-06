1b8643075e47768e494adad2cc333e7d9aa2abb7
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-07-06 11:10:16 -0300

    Add cycle list view model

---
+++ app/src/main/java/com/stahlt/cycleconfigurator/ui/cycle/list/CycleListViewModel.kt
@@ -0,0 +1,50 @@
+package com.stahlt.cycleconfigurator.ui.cycle.list
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.stahlt.cycleconfigurator.data.cycle.Cycle
+import com.stahlt.cycleconfigurator.data.cycle.network.ApiCycles
+import kotlinx.coroutines.launch
+
+data class CyclesListUIState(
+    val loading: Boolean = false ,
+    val hasError: Boolean = false ,
+    val cycles: List<Cycle> = listOf()
+) {
+    val success get():Boolean = !loading && !hasError
+}
+
+class CyclesListViewModel: ViewModel() {
+    private val tag: String = "CyclesListViewModel"
+    var uiState by mutableStateOf(CyclesListUIState())
+
+    init {
+        load()
+    }
+
+    fun load() {
+        uiState = uiState.copy(
+            loading = true,
+            hasError = false
+        )
+        viewModelScope.launch {
+            uiState = try {
+                val cycles = ApiCycles.retrofitService.findAll()
+                uiState.copy(
+                    loading = false,
+                    cycles = cycles
+                )
+            } catch (ex: Exception) {
+                Log.d(tag, "Error on Loading Cycles List")
+                uiState.copy(
+                    hasError = true,
+                    loading = false
+                )
+            }
+        }
+    }
+}
