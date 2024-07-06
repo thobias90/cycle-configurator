c23d1291c168a8b0931358e6924211f1ab5daa8b
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-07-06 10:29:55 -0300

    Add Cycle List

--- app/src/main/java/com/stahlt/cycleconfigurator/ui/cycle/list/CycleListScreen.kt
+++ app/src/main/java/com/stahlt/cycleconfigurator/ui/cycle/list/CycleListScreen.kt
@@ -1,23 +1,33 @@
 package com.stahlt.cycleconfigurator.ui.cycle.list
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.stahlt.cycleconfigurator.data.cycle.Cycle
 import com.stahlt.cycleconfigurator.ui.theme.CycleConfiguratorTheme
 
 @Composable
@@ -36,6 +46,7 @@ fun CycleListScreen(modifier: Modifier = Modifier) {
         Box(modifier = modifier.padding(paddingValues)) // just to not break
     }
 }
+
 @Preview
 @Composable
 fun CycleListScreenPreview(modifier: Modifier = Modifier) {
@@ -69,3 +80,93 @@ fun CyclesTopBarPreview(modifier: Modifier = Modifier) {
         CyclesTopBar()
     }
 }
+
+@Composable
+fun CyclesList(modifier: Modifier = Modifier ,cycles: List<Cycle>) {
+    if (cycles.isEmpty()) {
+        EmptyList(modifier)
+    } else {
+        // show filled list
+    }
+}
+
+@Composable
+fun EmptyList(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize() ,
+        verticalArrangement = Arrangement.Center ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "You don't have any cycles" ,
+            style = MaterialTheme.typography.titleLarge ,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Add cycles pressing \"+\"" ,
+            modifier = Modifier.padding(top = 8.dp) ,
+            style = MaterialTheme.typography.titleSmall ,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Preview(showBackground = true ,heightDp = 400)
+@Composable
+fun EmptyListPreview(modifier: Modifier = Modifier) {
+    CycleConfiguratorTheme {
+        EmptyList(modifier)
+    }
+}
+
+@Composable
+fun FilledList(modifier: Modifier = Modifier ,cycles: List<Cycle> ,onCyclePressed: () -> Unit) {
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(cycles) { index ,cycle->
+            ListItem(
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .clickable { onCyclePressed } ,
+                headlineContent = {
+                    Text(
+                        text = "${cycle.id} - ${cycle.name}" ,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                } ,
+                trailingContent = {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowForward ,
+                        contentDescription = "Select"
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilledListPreview(modifier: Modifier = Modifier) {
+    CycleConfiguratorTheme {
+        FilledList(modifier = modifier ,cycles = cyclesFake ,onCyclePressed = {})
+    }
+}
+
+val cyclesFake: List<Cycle> = listOf(
+    Cycle(
+        id = 1 ,
+        name = "Normal" ,
+        delay = 20 ,
+        temperature = "Hot" ,
+        spinSpeed = "Medium" ,
+        soilLevel = "Medium"
+    ) ,
+    Cycle(
+        id = 2 ,
+        name = "Quick" ,
+        delay = 0 ,
+        temperature = "Cold" ,
+        spinSpeed = "Slow" ,
+        soilLevel = "Low"
+    ) ,
+)
