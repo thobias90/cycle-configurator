dfc84ddd7e720c29adb8ebdb60d6a5d9e8cf27b9
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-07-06 08:59:50 -0300

    Add network interface with backend

---
+++ app/src/main/java/com/stahlt/cycleconfigurator/data/cycle/network/ApiCyclesService.kt
@@ -0,0 +1,38 @@
+package com.stahlt.cycleconfigurator.data.cycle.network
+
+import com.stahlt.cycleconfigurator.data.cycle.Cycle
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+private const val BASE_URL = "http://10.0.2.2:8080"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ApiCyclesService {
+    @GET("cycles")
+    suspend fun findAll(): List<Cycle>
+
+    @GET("cycles/{id}")
+    suspend fun findById(id: Int): Cycle
+
+    @DELETE("cycles/{id}")
+    suspend fun delete(id: Int)
+
+    @POST("cycles")
+    suspend fun save(@Body cycle: Cycle): Cycle
+}
+
+object ApiCycles {
+    val retrofitService: ApiCyclesService by lazy {
+        retrofit.create(ApiCyclesService::class.java)
+    }
+}
