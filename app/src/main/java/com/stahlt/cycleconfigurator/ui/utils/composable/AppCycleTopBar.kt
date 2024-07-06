e8b47be2f0af05c5f9fc3c9424f8fecac3634ac8
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-07-06 16:32:00 -0300

    Add common top bar composable

---
+++ app/src/main/java/com/stahlt/cycleconfigurator/ui/utils/composable/AppCycleTopBar.kt
@@ -0,0 +1,82 @@
+package com.stahlt.cycleconfigurator.ui.utils.composable
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.stahlt.cycleconfigurator.ui.theme.CycleConfiguratorTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppCycleTopBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    navigationIcon: Boolean = false,
+    saveIcon: Boolean = false,
+    refreshIcon: Boolean = false,
+    onBackPressed: () -> Unit,
+    onRefreshPressed: () -> Unit,
+    onSavePressed: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(title) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        ),
+        navigationIcon = {
+            if (navigationIcon) {
+                IconButton(onClick = onBackPressed) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack ,
+                        contentDescription = "back"
+                    )
+                }
+            }
+        },
+        actions = {
+            if (saveIcon) {
+                IconButton(onClick = onSavePressed) {
+                    Icon(
+                        imageVector = Icons.Filled.Check ,
+                        contentDescription = "save"
+                    )
+                }
+            }
+            if (refreshIcon) {
+                IconButton(onClick = onRefreshPressed) {
+                    Icon(
+                        imageVector = Icons.Filled.Refresh,
+                        contentDescription = "update"
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppCycleTopBarPreview(modifier: Modifier = Modifier) {
+    CycleConfiguratorTheme {
+        AppCycleTopBar(
+            title = "TopBar",
+            navigationIcon = true,
+            refreshIcon = true,
+            saveIcon = true,
+            onBackPressed = {},
+            onSavePressed = {},
+            onRefreshPressed = {}
+        )
+    }
+}
