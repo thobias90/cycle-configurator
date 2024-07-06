19b3a879f0c1ecb96384c2ab2777f75370dd91a9
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-07-06 16:32:09 -0300

    Update to use common top bar

--- app/src/main/java/com/stahlt/cycleconfigurator/ui/cycle/list/CycleListScreen.kt
+++ app/src/main/java/com/stahlt/cycleconfigurator/ui/cycle/list/CycleListScreen.kt
@@ -10,17 +10,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,6 +25,7 @@ import com.stahlt.cycleconfigurator.data.cycle.Cycle
 import com.stahlt.cycleconfigurator.ui.theme.CycleConfiguratorTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.stahlt.apppedidos.ui.utils.composables.DefaultErrorLoading
+import com.stahlt.cycleconfigurator.ui.utils.composable.AppCycleTopBar
 import com.stahlt.cycleconfigurator.ui.utils.composable.DefaultLoading
 
 @Composable
@@ -40,80 +36,69 @@ fun CycleListScreen(
     onCyclePressed: (Cycle) -> Unit
 ) {
     Scaffold(
-        modifier = modifier.fillMaxSize() ,
+        modifier = modifier.fillMaxSize(),
         topBar = {
-            CyclesTopBar(
-                modifier ,
-                onRefresh = viewModel::load
+            AppCycleTopBar(
+                title = "Cycles",
+                refreshIcon = true,
+                onRefreshPressed = viewModel::load,
+                onBackPressed = {},
+                onSavePressed = {}
             )
-        } ,
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = onAddPressed) {
-                Icon(imageVector = Icons.Default.Add ,contentDescription = "Add")
+                Icon(imageVector = Icons.Default.Add,contentDescription = "Add")
             }
         }
     ) { paddingValues->
         if (viewModel.uiState.loading) {
             DefaultLoading(
-                modifier = modifier ,
+                modifier = modifier,
                 text = "Loading"
             )
         } else if (viewModel.uiState.hasError) {
             DefaultErrorLoading(
-                modifier = modifier ,
-                text = "Unable to load cycles" ,
+                modifier = modifier,
+                text = "Unable to load cycles",
                 onTryAgainPressed = viewModel::load
             )
         } else {
             CyclesList(
-                modifier = modifier.padding(paddingValues) ,
-                cycles = viewModel.uiState.cycles ,
+                modifier = modifier.padding(paddingValues),
+                cycles = viewModel.uiState.cycles,
                 onCyclePressed = onCyclePressed
             )
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CyclesTopBar(modifier: Modifier = Modifier, onRefresh: () -> Unit) {
-    TopAppBar(
-        modifier = modifier ,
-        title = { Text("Cycles") } ,
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer ,
-            titleContentColor = MaterialTheme.colorScheme.primary
-        ) ,
-        actions = {
-            IconButton(onClick = onRefresh) {
-                Icon(imageVector = Icons.Filled.Refresh ,contentDescription = "Update")
-            }
-        }
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun CyclesTopBarPreview(modifier: Modifier = Modifier) {
     CycleConfiguratorTheme {
-        CyclesTopBar(
-            onRefresh = {}
+        AppCycleTopBar(
+            title = "Cycles",
+            refreshIcon = true,
+            onRefreshPressed = {},
+            onSavePressed = {},
+            onBackPressed = {}
         )
     }
 }
 
 @Composable
 fun CyclesList(
-    modifier: Modifier = Modifier ,
-    cycles: List<Cycle> ,
+    modifier: Modifier = Modifier,
+    cycles: List<Cycle>,
     onCyclePressed: (Cycle) -> Unit
 ) {
     if (cycles.isEmpty()) {
         EmptyList(modifier)
     } else {
         FilledList(
-            modifier ,
-            cycles ,
+            modifier,
+            cycles,
             onCyclePressed = onCyclePressed
         )
     }
@@ -122,25 +107,25 @@ fun CyclesList(
 @Composable
 fun EmptyList(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize() ,
-        verticalArrangement = Arrangement.Center ,
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "You don't have any cycles" ,
-            style = MaterialTheme.typography.titleLarge ,
+            text = "You don't have any cycles",
+            style = MaterialTheme.typography.titleLarge,
             color = MaterialTheme.colorScheme.primary
         )
         Text(
-            text = "Add cycles pressing \"+\"" ,
-            modifier = Modifier.padding(top = 8.dp) ,
-            style = MaterialTheme.typography.titleSmall ,
+            text = "Add cycles pressing \"+\"",
+            modifier = Modifier.padding(top = 8.dp),
+            style = MaterialTheme.typography.titleSmall,
             color = MaterialTheme.colorScheme.primary
         )
     }
 }
 
-@Preview(showBackground = true ,heightDp = 400)
+@Preview(showBackground = true,heightDp = 400)
 @Composable
 fun EmptyListPreview(modifier: Modifier = Modifier) {
     CycleConfiguratorTheme {
@@ -149,22 +134,22 @@ fun EmptyListPreview(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun FilledList(modifier: Modifier = Modifier ,cycles: List<Cycle> ,onCyclePressed: (Cycle) -> Unit) {
+fun FilledList(modifier: Modifier = Modifier,cycles: List<Cycle>,onCyclePressed: (Cycle) -> Unit) {
     LazyColumn(modifier = modifier) {
-        items(cycles) {  cycle->
+        items(cycles) { cycle->
             ListItem(
                 modifier = Modifier
                     .padding(vertical = 4.dp)
-                    .clickable { onCyclePressed(cycle) } ,
+                    .clickable { onCyclePressed(cycle) },
                 headlineContent = {
                     Text(
-                        text = "${cycle.id} - ${cycle.name}" ,
+                        text = "${cycle.id} - ${cycle.name}",
                         color = MaterialTheme.colorScheme.primary
                     )
-                } ,
+                },
                 trailingContent = {
                     Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowForward ,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowForward,
                         contentDescription = "Select"
                     )
                 }
@@ -177,25 +162,25 @@ fun FilledList(modifier: Modifier = Modifier ,cycles: List<Cycle> ,onCyclePresse
 @Composable
 fun FilledListPreview(modifier: Modifier = Modifier) {
     CycleConfiguratorTheme {
-        FilledList(modifier = modifier ,cycles = cyclesFake ,onCyclePressed = {})
+        FilledList(modifier = modifier,cycles = cyclesFake,onCyclePressed = {})
     }
 }
 
 val cyclesFake: List<Cycle> = listOf(
     Cycle(
-        id = 1 ,
-        name = "Normal" ,
-        delay = 20 ,
-        temperature = "Hot" ,
-        spinSpeed = "Medium" ,
+        id = 1,
+        name = "Normal",
+        delay = 20,
+        temperature = "Hot",
+        spinSpeed = "Medium",
         soilLevel = "Medium"
-    ) ,
+    ),
     Cycle(
-        id = 2 ,
-        name = "Quick" ,
-        delay = 0 ,
-        temperature = "Cold" ,
-        spinSpeed = "Slow" ,
+        id = 2,
+        name = "Quick",
+        delay = 0,
+        temperature = "Cold",
+        spinSpeed = "Slow",
         soilLevel = "Low"
-    ) ,
+    ),
 )
